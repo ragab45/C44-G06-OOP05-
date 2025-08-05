15fcc301ab3e349b4f19341368d919be55b9143e
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-08-05 19:10:03 +0300

    part-1

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region part-1
+            /*
+            1- What is the primary purpose of an interface in C#?
+              ans -   To define a blueprint for a class
+            2-Which of the following is NOT a valid access modifier for interface members in C#?
+                ans -   private
+            3- Can an interface contain fields in C#?
+                ans -   No, interfaces cannot contain fields
+            4-In C#, can an interface inherit from another interface?
+              ans -   Yes, an interface can inherit from another interface
+            5-Which keyword is used to implement an interface in a class in C#?
+               ans -   The 'implements' 
+            6-Can an interface contain static methods in C#?
+               ans -   No, interfaces cannot contain static methods
+            7- In C#, can an interface have explicit access modifiers for its members?
+                           ans -   No, No, all members are implicitly public
+            8- What is the purpose of an explicit interface implementation in C#?
+                ans -   b) To provide a clear separation between interface and class members
+            9-In C#, can an interface have a constructor?
+               ans -   No, interfaces cannot have constructors
+            10-How can a C# class implement multiple interfaces?
+                ans -   By separating interface names with commas   
+             */
+            #endregion
         }
     }
 }
